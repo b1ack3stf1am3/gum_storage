ee1194f658ac28bdf8aeeb285e188041d50ba86f
unknown <jgriger870@wit-xdvm-066.MILLARDPS.NET>
2013-01-31 12:14:23 -0600

    Still and unknown topic

---
+++ index.php
@@ -0,0 +1,27 @@
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+		<script type="text/javascript" src="bootstrap/js/bootstrap.js"></script>
+		<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css" />
+		<link rel="stylesheet" type="text/css" href="styles.css" />
+		<title>Unknown</title>
+	</head>
+	<body>
+		<div id="wrapper" class="container">
+			<header>
+				<?php include('layout/header.php')?>
+			</header>
+			<nav>
+				<?php include('layout/nav.php')?>
+			</nav>
+			<section role="main">
+				<?php include('layout/main.php')?>
+			</section>
+			<footer>
+				<?php include('layout/footer.php')?>
+			</footer>
+		</div>
+	</body>
+</html>
